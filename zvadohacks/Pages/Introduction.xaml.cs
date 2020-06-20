3862ec9537d8ebd211fa31f7426f6e410b1d33c7
viktor111 <swifrorlilko@gmail.com>
2020-06-20 22:48:24 +0300

    add images

--- zvadohacks/Pages/Introduction.xaml.cs
+++ zvadohacks/Pages/Introduction.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-
 using Xamarin.Forms;
 
 namespace zvadohacks.Pages
@@ -11,5 +9,16 @@ namespace zvadohacks.Pages
         {
             InitializeComponent();
         }
+
+        void Cell_1_Tapped(object sender, EventArgs e)
+        {
+            const string Detail = "You will learn what is kali how to install and set it up.";       
+            CellInstallingKali.Detail = Detail;                      
+        }
+        void Cell_2_Tapped(object sender, EventArgs e)
+        {
+            const string Detail = "You will learn how to install and use zvado hacking tools";
+            CellUsingZvadoTool.Detail = Detail;
+        }
     }
-}
+}   
